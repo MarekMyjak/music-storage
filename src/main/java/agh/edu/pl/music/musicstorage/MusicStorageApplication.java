7935f90818fc8e93418613be593222af6217400a
Marek Myjak <mmyjak@d17-l427-pc-11.ipa.iisg.agh.edu.pl>
2018-05-07 18:05:09 +0200

    first commit

---
+++ src/main/java/agh/edu/pl/music/musicstorage/MusicStorageApplication.java
@@ -0,0 +1,12 @@
+package agh.edu.pl.music.musicstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicStorageApplication.class, args);
+	}
+}
